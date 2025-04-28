79f592f8b5d52b68e4140d284a1fec5e4eac0904
miljatase <Milja.Taskinen2@metropolia.fi>
2025-04-28 16:02:01 +0300

    Historia kysymysten lisäys

--- js/kategories/historia.js
+++ js/kategories/historia.js
@@ -1,182 +1,263 @@
 const questions = [
     {
-        question: "What is the smallest prime number?",
+        question: "Kuka oli Rooman valtakunnan ensimmäinen keisari?",
         answers: [
-            { text: "0", correct: false },
-            { text: "1", correct: false },
-            { text: "2", correct: true },
-            { text: "3", correct: false },
+            { text: "Julius Caesar", correct: false },
+            { text: "Nero", correct: false },
+            { text: "Augustus", correct: true },
+            { text: "Tiberius", correct: false },
         ]
     },
     {
-        question: "Which planet is known as the Red Planet?",
+        question: "Kuka allekirjoitti Yhdysvaltain itsenäisyysjulistuksen ensimmäisenä?",
         answers: [
-            { text: "Mercury", correct: false },
-            { text: "Mars", correct: true },
-            { text: "Jupiter", correct: false },
-            { text: "Venus", correct: false },
+            { text: "George Washington", correct: false },
+            { text: "John Hancock", correct: true },
+            { text: "Thomas Jefferson", correct: false },
+            { text: "John Adams", correct: false },
         ]
     },
     {
-        question: "What is the chemical symbol for water?",
+        question: "Mikä oli Silkkitie?",
         answers: [
-            { text: "HO", correct: false },
-            { text: "O₂", correct: false },
-            { text: "H₂O", correct: true },
-            { text: "OH₂", correct: false },
+            { text: "Kauppareittien verkosto, joka yhdisti idän ja lännen.", correct: true },
+            { text: "Muinainen merireitti Aasiaan.", correct: false },
+            { text: "Kiinan keisarien rakentama puolustusmuuri.", correct: false },
+            { text: "Roomalaisten rakentama tieverkosto Euroopassa.", correct: false },
         ]
     },
     {
-        question: "Which language has the most native speakers worldwide?",
+        question: "Minä vuonna Kolumbus saapui Amerikkaan?",
         answers: [
-            { text: "English", correct: false },
-            { text: "Mandarin Chinese", correct: true },
-            { text: "Spanish", correct: false },
-            { text: "Hindi", correct: false },
+            { text: "1488", correct: false },
+            { text: "1501", correct: false },
+            { text: "1519", correct: false },
+            { text: "1492", correct: true },
         ]
     },
     {
-        question: "What currency is used in Japan?",
+        question: "Mikä oli Berliinin muuri ja milloin se murtui?",
         answers: [
-            { text: "Yuan", correct: false },
-            { text: "Yen", correct: true },
-            { text: "Won", correct: false },
-            { text: "Dollar", correct: false },
+            { text: "Muuri, joka erotti Itä- ja Länsi-Saksan toisen maailmansodan aikana, murtui 1949.", correct: false },
+            { text: "Kylmän sodan aikainen raja-aita Berliinissä, murtui 1989.", correct: true },
+            { text: "Muinainen puolustusmuuri Berliinin ympärillä, murtui 1871.", correct: false },
+            { text: "Ensimmäisen maailmansodan aikainen rintamalinja, murtui 1918.", correct: false },
         ]
     },
     {
-        question: "In which sport is the term “love” used to indicate a score of zero?",
+        question: "Minä vuonna alkoi toinen maailmansota?",
         answers: [
-            { text: "Badminton", correct: false },
-            { text: "Tennis", correct: true },
-            { text: "Cricket", correct: false },
-            { text: "Basketball", correct: false },
+            { text: "1937", correct: false },
+            { text: "1935", correct: false },
+            { text: "1939", correct: true },
+            { text: "1941", correct: false },
         ]
     },
     {
-        question: "What is the capital city of Canada?",
+        question: "Kuka oli ensimmäinen Yhdysvaltain presidentti?",
         answers: [
-            { text: "Toronto", correct: false },
-            { text: "Ottawa", correct: true },
-            { text: "Vancouver", correct: false },
-            { text: "Montreal", correct: false },
+            { text: "Thomas Jefferson", correct: false },
+            { text: "George Washington", correct: true },
+            { text: "Benjamin Franklin", correct: false },
+            { text: "Abraham Lincoln", correct: false },
         ]
     },
     {
-        question: "Which organ pumps blood around the human body?",
+        question: "Mikä oli renessanssin keskeinen aate?",
         answers: [
-            { text: "Brain", correct: false },
-            { text: "Liver", correct: false },
-            { text: "Heart", correct: true },
-            { text: "Lung", correct: false },
+            { text: "Humanismi", correct: true },
+            { text: "Teokratia", correct: false },
+            { text: "Asketismi", correct: false },
+            { text: "Feodalismi", correct: false },
         ]
     },
     {
-        question: "Who painted the Mona Lisa?",
+        question: "Kuka oli kuuluisa brittiläinen tiedemies, joka muotoili painovoimalain?",
         answers: [
-            { text: "Vincent van Gogh", correct: false },
-            { text: "Pablo Picasso", correct: false },
+            { text: "Albert Einstein", correct: false },
+            { text: "Galileo Galilei", correct: false },
+            { text: "Stephen Hawking", correct: false },
+            { text: "Isaac Newton", correct: true },
+        ]
+    },
+    {
+        question: "Kuka oli antiikin Kreikan filosofi, joka opetti Aleksanteri Suurta?",
+        answers: [
+            { text: "Sokrates", correct: false },
+            { text: "Platon", correct: false },
+            { text: "Aristoteles", correct: true },
+            { text: "Pythagoras", correct: false },
+        ]
+    },
+    {
+        question: "Mikä oli ensimmäinen ihmisen lähettämä satelliitti Maan kiertoradalle?",
+        answers: [
+            { text: "Explorer 1", correct: false },
+            { text: "Apollo 11", correct: false },
+            { text: "Sputnik 1", correct: true },
+            { text: "Voyager 1", correct: false },
+        ]
+    },
+    {
+        question: "Kuka oli kuuluisa italialainen taiteilija ja keksijä renessanssin aikana?",
+        answers: [
             { text: "Leonardo da Vinci", correct: true },
-            { text: "Claude Monet", correct: false },
+            { text: "Michelangelo", correct: false },
+            { text: "Raffaello", correct: false },
+            { text: "Donatello", correct: false },
         ]
     },
     {
-        question: "How many continents are there on Earth?",
+        question: "Mikä oli Neuvostoliiton johtama sotilasliitto kylmän sodan aikana?",
         answers: [
-            { text: "Five", correct: false },
-            { text: "Six", correct: false },
-            { text: "Seven", correct: true },
-            { text: "Eight", correct: false },
+            { text: "Nato", correct: false },
+            { text: "SEATO", correct: false },
+            { text: "Baltian liitto", correct: false },
+            { text: "Varsovan liitto", correct: true },
         ]
     },
     {
-        question: "Which gas do plants absorb during photosynthesis?",
+        question: "Kuka oli kuuluisa saksalainen fyysikko, joka kehitti suhteellisuusteorian?",
         answers: [
-            { text: "Oxygen", correct: false },
-            { text: "Nitrogen", correct: false },
-            { text: "Carbon dioxide", correct: true },
-            { text: "Hydrogen", correct: false },
+            { text: "Werner Heisenberg", correct: false },
+            { text: "Albert Einstein", correct: true },
+            { text: "Max Planck", correct: false },
+            { text: "Erwin Schrödinger", correct: false },
         ]
     },
     {
-        question: "What is the hardest natural substance on Earth?",
+        question: "Mikä maa isännöi ensimmäisiä moderneja olympialaisia?",
         answers: [
-            { text: "Gold", correct: false },
-            { text: "Iron", correct: false },
-            { text: "Diamond", correct: true },
-            { text: "Quartz", correct: false },
+            { text: "Italia", correct: false },
+            { text: "Kreikka", correct: true },
+            { text: "Ranska", correct: false },
+            { text: "Yhdysvallat", correct: false },
         ]
     },
     {
-        question: "Which ocean is the largest by area?",
+        question: "Ketkä olivat ”kylmän sodan” kaksi suurta vastustajaa?",
         answers: [
-            { text: "Atlantic Ocean", correct: false },
-            { text: "Indian Ocean", correct: false },
-            { text: "Arctic Ocean", correct: false },
-            { text: "Pacific Ocean", correct: true },
+            { text: "Yhdysvallat ja Neuvostoliitto", correct: true },
+            { text: "Saksa ja Ranska", correct: false },
+            { text: "Iso-Britannia ja Japani", correct: false },
+            { text: "Kiina ja Intia", correct: false },
         ]
     },
     {
-        question: "What is 3 squared?",
+        question: "Mitä tapahtui Titanicilla vuonna 1912?",
         answers: [
-            { text: "6", correct: false },
-            { text: "9", correct: true },
-            { text: "12", correct: false },
-            { text: "3", correct: false },
+            { text: "Se syttyi tuleen Atlantilla", correct: false },
+            { text: "Se saapui onnistuneesti New Yorkiin ennätysajassa", correct: false },
+            { text: "Se joutui merirosvojen hyökkäyksen kohteeksi", correct: false },
+            { text: "Se upposi törmättyään jäävuoreen", correct: true },
         ]
     },
     {
-        question: "Which musical instrument typically has 88 keys?",
+        question: "Mikä oli Suuri lama?",
         answers: [
-            { text: "Guitar", correct: false },
-            { text: "Violin", correct: false },
-            { text: "Piano", correct: true },
-            { text: "Flute", correct: false },
+            { text: "Pitkäaikainen nälänhätä Irlannissa.", correct: false },
+            { text: "Vakava maailmanlaajuinen talouskriisi 1930-luvulla.", correct: true },
+            { text: "Suuri tulipalo Lontoossa 1600-luvulla.", correct: false },
+            { text: "Ranskan vallankumouksen jälkeinen epävakaa aika.", correct: false },
         ]
     },
     {
-        question: "Who is the author of the 'Harry Potter' series?",
+        question: "Kuka oli Leonardo da Vinci?",
         answers: [
-            { text: "C.S. Lewis", correct: false },
-            { text: "J.R.R. Tolkien", correct: false },
-            { text: "J.K. Rowling", correct: true },
-            { text: "Stephen King", correct: false },
+            { text: "Kuuluisa italialainen kuvanveistäjä.", correct: false },
+            { text: "Espanjalainen löytöretkeilijä.", correct: false },
+            { text: "Italialainen taidemaalari", correct: true },
+            { text: "Englantilainen kirjailija.", correct: false },
         ]
     },
     {
-        question: "Which animal is commonly called the 'King of the Jungle'?",
+        question: "Kuka oli Marco Polo?",
         answers: [
-            { text: "Tiger", correct: false },
-            { text: "Elephant", correct: false },
-            { text: "Lion", correct: true },
-            { text: "Gorilla", correct: false },
-        ]
-    },
-    {
-        question: "In computing, what does the abbreviation 'www' stand for?",
-        answers: [
-            { text: "World Web Window", correct: false },
-            { text: "World Wide Web", correct: true },
-            { text: "Web World Wide", correct: false },
-            { text: "Wide World Website", correct: false },
-        ]
-    },
-    {
-        question: "Which metal is liquid at room temperature?",
-        answers: [
-            { text: "Silver", correct: false },
-            { text: "Mercury", correct: true },
-            { text: "Copper", correct: false },
-            { text: "Aluminum", correct: false },
-        ]
-    },
-    {
-        question: "Which country gifted the Statue of Liberty to the United States?",
-        answers: [
-            { text: "United Kingdom", correct: false },
-            { text: "France", correct: true },
-            { text: "Spain", correct: false },
-            { text: "Germany", correct: false },
+            { text: "Venetsialainen tutkimusmatkailija", correct: true },
+            { text: "Kreikkalainen filosofi.", correct: false },
+            { text: "Roomalainen keisari.", correct: false },
+            { text: " Portugalilainen merenkulkija.", correct: false },
         ]
     }
+    {
+        question: "Mikä oli Euroopan unionin perustamisen tarkoitus?",
+        answers: [
+            { text: "Palauttaa Euroopan siirtomaavalta.", correct: false },
+            { text: "Aloittaa uusi maailmansota.", correct: false },
+            { text: "Luoda yhtenäinen eurooppalainen armeija puolustamaan valtioita.", correct: false },
+            { text: "Edistää taloudellista yhteistyötä Euroopan valtioiden välillä.", correct: true },
+        ]
+    },
+    {
+        question: "Mitä tapahtui suuressa palossa Lontoossa 1666?",
+        answers: [
+            { text: "Lontoo tuhoutui maanjäristyksessä.", correct: false },
+            { text: "Viikingit hyökkäsivät Lontooseen.", correct: false },
+            { text: "Suuri osa Lontoon keskustaa tuhoutui tulipalossa.", correct: true },
+            { text: "Musta surma saapui Lontooseen.", correct: false },
+        ]
+    },
+    {
+        question: "Milloin holokausti, yksi historian kauheimmista tapahtumista, tapahtui?",
+        answers: [
+            { text: "Ensimmäisen maailmansodan aikana", correct: false },
+            { text: "Toisen maailmansodan aikana", correct: true },
+            { text: "Kylmän sodan aikana", correct: false },
+            { text: "Napoleonin sotien aikana", correct: false },
+        ]
+    },
+    {
+        question: "Mikä oli Naton etunimi ennen sen nykyistä nimeä?",
+        answers: [
+            { text: "Pohjois-Atlantin sopimus", correct: true },
+            { text: "Atlantin liitto", correct: false },
+            { text: "Euroopan puolustusliitto", correct: false },
+            { text: "Länsiliitto", correct: false },
+        ]
+    },
+    {
+        question: "Mitä dynastiaa pidettiin Kiinan historian kulta-ajana?",
+        answers: [
+            { text: "Han-dynastia", correct: false },
+            { text: "Ming-dynastia", correct: false },
+            { text: "Qing-dynastia", correct: false },
+            { text: "Tang-dynastia", correct: true },
+        ]
+    },
+    {
+        question: "Kuka piti kuuluisan "Minulla on unelma" -puheen?",
+        answers: [
+            { text: "Abraham Lincoln", correct: false },
+            { text: "Martin Luther King Jr.", correct: true },
+            { text: "Nelson Mandela", correct: false },
+            { text: "Barack Obama", correct: false },
+        ]
+    },
+    {
+        question: "Milloin Brexit tapahtui?",
+        answers: [
+            { text: "Kesäkuu 2016", correct: false },
+            { text: "Maaliskuu 2019", correct: false },
+            { text: "Tammikuu 2020", correct: true },
+            { text: "Joulukuu 2020", correct: false },
+        ]
+    },
+    {
+        question: "Mistä Vapaudenpatsas on peräisin?",
+        answers: [
+            { text: "Ranska", correct: true },
+            { text: "Iso-Britannia", correct: false },
+            { text: "Yhdysvallat", correct: false },
+            { text: "Italia", correct: false },
+        ]
+    },
+    {
+        question: "Minä vuonna Facebook perustettiin?",
+        answers: [
+            { text: "1998", correct: false },
+            { text: "2001", correct: false },
+            { text: "2007", correct: false },
+            { text: "2004", correct: true },
+        ]
+    },
 ];
