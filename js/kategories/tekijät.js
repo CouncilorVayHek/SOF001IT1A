41ca6d589a816e68b987d450846e32ab372d844d
miljatase <Milja.Taskinen2@metropolia.fi>
2025-04-28 17:14:29 +0300

    Milja kysymykset

--- js/kategories/tekijät.js
+++ js/kategories/tekijät.js
@@ -8,4 +8,31 @@ const questions= [
             { text: "", correct: false },
         ]
     },
+    {
+        question: "Mikä on Miljan lempiruoka?",
+        answers: [
+            { text: "Kalakeitto", correct: true },
+            { text: "Sienirisotto", correct: false },
+            { text: "Jauhelihakeitto", correct: false },
+            { text: "Uunimakkara", correct: false },
+        ]
+    },
+    {
+        question: "Mistä Milja on kotoisin?",
+        answers: [
+            { text: "Kuopio", correct: false },
+            { text: "Oulu", correct: false },
+            { text: "Kajaani", correct: true },
+            { text: "Seinäjoki", correct: false },
+        ]
+    },
+    {
+        question: "Mikä on Miljan lempisarja?",
+        answers: [
+            { text: "what we do in the shadows", correct: false },
+            { text: "Friends", correct: true },
+            { text: "Supernatural", correct: false },
+            { text: "Brooklyn nine-nine", correct: false },
+        ]
+    },
 ];
